6fd14f2f04baebeee689daccc823d300e9e75b7f
Ruchir Mandora <63937997+ruchirm702@users.noreply.github.com>
2023-11-20 21:52:54 +0530

    Update 3. Leaders in an array  ..java

--- 3. Leaders in an array  ..java
+++ 3. Leaders in an array  ..java
@@ -11,43 +11,53 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
+    // Function to find leaders in an array
     public static int[] solve(int[] A) {
-        int count = 0;                        // Counter for leaders
-        int max = A[A.length - 1];            // Initialize the maximum as the rightmost element
-        int[] leaders = new int[A.length];    // Create an array to store leader elements
-        
-        // The rightmost element is always a leader
-        leaders[count++] = max;
+        // ArrayList to store leader elements
+        ArrayList<Integer> leaders = new ArrayList<>();
+        // Length of the array
+        int n = A.length;
+        // Initialize the maximum with the rightmost element
+        int max = A[n - 1];
+
+        // Add the rightmost element to leaders
+        leaders.add(max);
 
         // Iterate through the array from right to left
-        for (int i = A.length - 2; i >= 0; i--) {
-            if (A[i] > max) {
-                max = A[i];
-                leaders[count++] = max;      // The current element is a leader
+        for (int i = n - 2; i >= 0; i--) {
+            int ele = A[i];
+            // If the current element is greater than the maximum, it is a leader
+            if (ele > max) {
+                leaders.add(ele);
+                max = ele;
             }
         }
 
-        // Create a result array with the correct size
-        int[] result = new int[count];
-        for (int i = 0; i < count; i++) {
-            result[i] = leaders[count - i - 1]; // Copy leader elements to the result array
+        // Convert ArrayList to array
+        int m = leaders.size();
+        int[] result = new int[m];
+        for (int i = 0; i < m; i++) {
+            result[i] = leaders.get(i);
         }
-        
-        return result; // Return the array of leader elements
+
+        // Return the array of leader elements
+        return result;
     }
 
     public static void main(String[] args) {
-    int[] A = {16, 17, 4, 3, 5, 2};
-    int[] result = solve(A); // Call the static solve method
+        // Example array
+        int[] A = {16, 17, 4, 3, 5, 2};
+        // Call the solve method to find leaders
+        int[] result = solve(A);
 
-    // Print the result array with square brackets
-    System.out.print("[");
-    for (int i = 0; i < result.length; i++) {
-        System.out.print(result[i]);
-        if (i < result.length - 1) {
-            System.out.print(", ");
+        // Print the result array with square brackets
+        System.out.print("[");
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]);
+            if (i < result.length - 1) {
+                System.out.print(", ");
+            }
         }
-    }
         System.out.println("]");
-   }
+    }
 }
